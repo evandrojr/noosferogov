390bcf53128697491686b2a5a1de4382dced156c
Evandro Jr <evandrojr@gmail.com>
2015-12-30 16:31:56 -0300

    Adds Video gallery and other video stuff (like video article and video gallery block)

---
+++ plugins/video/test/unit/video_plugin_test.rb
@@ -0,0 +1,8 @@
+require_relative '../test_helper'
+class VideoPluginTest < ActiveSupport::TestCase
+
+  should "return VideoBlock in extra_blocks class method" do
+    assert  VideoPlugin.extra_blocks.keys.include?(VideoPlugin::VideoBlock)
+  end
+
+end
