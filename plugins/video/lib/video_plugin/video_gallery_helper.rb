8e8fddc28176a48bef14812162a4b29c1fddb92c
Evandro Jr <evandrojr@gmail.com>
2016-01-04 16:53:07 -0300

    Changes for rails4

--- plugins/video/lib/video_plugin/video_gallery_helper.rb
+++ plugins/video/lib/video_plugin/video_gallery_helper.rb
@@ -5,10 +5,9 @@ module VideoPlugin::VideoGalleryHelper
       configure[:list_type] ||= :folder
       if !configure[:contents].blank?
         configure[:contents] = configure[:contents].paginate(
-          :order => "updated_at DESC",
           :per_page => 17,
           :page => params[:npage]
-        )
+        ).order("updated_at DESC")
         render :file => 'shared/video_list', :locals => configure
       else
         content_tag('em', _('(empty folder)'))
