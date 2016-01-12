7dbcc8710f863d98c75f9e428756d4d403b816d7
Serpro Designer <designer@serpro.gov.br>
2016-01-12 22:50:08 +0000

    backup designer

---
+++ public/designs/themes/teste-will/theme.js
@@ -0,0 +1,7 @@
+﻿$( document ).ready(function() {
+	//move a busca para o primeiro block 'block-outer'
+   $('.action-home-index #user #top-search').appendTo('.box-1 .blocks .block-outer:nth-child(1) .box');
+   //troca para português o termo de busca
+   $('.action-home-index #top-search input').attr( 'title', 'BUSCAR NO PORTAL' ); 
+   $('.action-home-index #top-search input').attr( 'value', 'BUSCAR NO PORTAL' ); 
+});
