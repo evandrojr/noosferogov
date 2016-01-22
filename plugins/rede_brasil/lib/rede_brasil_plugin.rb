d85b0b2d26d8b6e80d9778002b15f00bc6e6fcd8
Evandro Junior <evandrojr@gmail.com>
2016-01-22 15:51:30 -0300

    saving work

---
+++ plugins/rede_brasil/lib/rede_brasil_plugin.rb
@@ -0,0 +1,13 @@
+class RedeBrasilPlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    # FIXME
+    "RedeBrasilPlugin"
+  end
+
+  def self.plugin_description
+    # FIXME
+    _("A plugin that does this and that.")
+  end
+
+end
