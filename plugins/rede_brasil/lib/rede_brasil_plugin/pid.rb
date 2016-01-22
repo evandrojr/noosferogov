1d3f6910c9d47bd181dabe877d2f1c2681da1634
Evandro Junior <evandrojr@gmail.com>
2016-01-22 16:43:49 -0300

    Added questions

--- plugins/rede_brasil/lib/rede_brasil_plugin/pid.rb
+++ plugins/rede_brasil/lib/rede_brasil_plugin/pid.rb
@@ -1,17 +1,17 @@
-class RedeBrasilPlugin::Pid < Community
-
-  def self.type_name
-    _('Pid')
-  end
-
-  @e1 = Environment.default
-
-  @community = create(Community, :environment => @e1, :name => 'pid test')
-
-  @community_custom_field = CustomField.create(:name => "community_field", :format=>"myFormat", :default_value => "value for community", :customized_type=>"Community", :active => true, :environment => @e1)
-  # @person_custom_field = CustomField.create(:name => "person_field", :format=>"myFormat", :default_value => "value for person", :customized_type=>"Person", :active => true, :environment => @e1)
-  # @profile_custom_field = CustomField.create(:name => "profile_field", :format=>"myFormat", :default_value => "value for any profile", :customized_type=>"Profile", :active => true, :environment => @e1)
-
-
-
-end
+# class RedeBrasilPlugin::Pid < Community
+#
+#   def self.type_name
+#     _('Pid')
+#   end
+#
+#   # @e1 = Environment.default
+#   #
+#   # @community = create(Community, :environment => @e1, :name => 'pid test')
+#   #
+#   # @community_custom_field = CustomField.create(:name => "community_field", :format=>"myFormat", :default_value => "value for community", :customized_type=>"Community", :active => true, :environment => @e1)
+#   # # @person_custom_field = CustomField.create(:name => "person_field", :format=>"myFormat", :default_value => "value for person", :customized_type=>"Person", :active => true, :environment => @e1)
+#   # # @profile_custom_field = CustomField.create(:name => "profile_field", :format=>"myFormat", :default_value => "value for any profile", :customized_type=>"Profile", :active => true, :environment => @e1)
+#
+#
+#
+# end
